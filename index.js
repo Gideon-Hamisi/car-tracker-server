430d8a32b41112b0f9ab08ed2e6f35a8da96b5ae
Neocipher Technologies <neociphertechnologies @gmail.com>
2025-03-17 14:38:31 +0300

    Ready for Railway deploymen

--- index.js
+++ index.js
@@ -56,6 +56,6 @@ app.get('/', async (req, res) => {
   `);
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT || port}`);
 });
